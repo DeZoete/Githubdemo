a36882c667b4c738317ed334ea4932cc503a27f9
FrederikMoestrup <cph-fm124@cphbusiness.dk>
2023-10-27 12:10:05 +0200

    Create Conflict.java

---
+++ src/main/java/org/example/Conflict.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Conflict {
+
+    Conflict(){
+
+    }
+
+    public void mergeConflict(){
+        System.out.println("Fred 1 er stadig nummer 1.");
+    }
+
+}
