8b52c1e872a20da0ee870407a241e2e0e3234bc2
DeZoete <143606680+DeZoete@users.noreply.github.com>
2023-10-27 12:04:26 +0200

    update main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Fred 2 will be demoted");
-
+        System.out.println("cock");
     }
 }
